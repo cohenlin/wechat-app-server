38d3d3d53ee090607639c1cc0c67f1414b22c094
cohen <18264180308@163.com>
2018-11-03 14:09:31 +0800

    修改测试接口名

--- src/main/java/com/cohen/wechat/app/server/controller/BasicApiController.java
+++ src/main/java/com/cohen/wechat/app/server/controller/BasicApiController.java
@@ -1,12 +1,10 @@
 package com.cohen.wechat.app.server.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/basic/api")
+//@RequestMapping("/basic/api")
 public class BasicApiController {
 
     @GetMapping("/hello")
