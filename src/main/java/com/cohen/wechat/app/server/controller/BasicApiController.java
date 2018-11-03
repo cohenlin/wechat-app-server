fc17f23c053c1fe608c828f62b6b51c28b69ff83
cohen <18264180308@163.com>
2018-11-03 12:33:53 +0800

    测试接口改为get请求

--- src/main/java/com/cohen/wechat/app/server/controller/BasicApiController.java
+++ src/main/java/com/cohen/wechat/app/server/controller/BasicApiController.java
@@ -1,5 +1,6 @@
 package com.cohen.wechat.app.server.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/basic/api")
 public class BasicApiController {
 
-    @PostMapping("/hello")
+    @GetMapping("/hello")
     public String hello(){
         return "I'm wechat-app-server";
     }
