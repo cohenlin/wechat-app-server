ac2233e38b61d7077dba84da37f7d6044ee396cc
linjincheng <ljc@choicesoft.com.cn>
2018-11-15 17:24:45 +0800

    编写任务创建接口

--- src/main/java/com/cohen/wechat/app/server/controller/BasicApiController.java
+++ src/main/java/com/cohen/wechat/app/server/controller/BasicApiController.java
@@ -1,10 +1,11 @@
 package com.cohen.wechat.app.server.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-//@RequestMapping("/basic/api")
+@RequestMapping("/api")
 public class BasicApiController {
 
     @GetMapping("/hello")
--- src/test/java/com/cohen/wechat/app/server/ApiTest.java
+++ src/test/java/com/cohen/wechat/app/server/ApiTest.java
@@ -1,21 +1,10 @@
 package com.cohen.wechat.app.server;
 
-import com.cohen.wechat.app.server.controller.WechatServerController;
-import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
 public class ApiTest {
-
-    @Autowired
-    private WechatServerController controller;
-
-    @Test
-    public void test(){
-        controller.ping("hello");
-    }
 }
